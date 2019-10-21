704301507d9884777d33c41adc87d9683818c8c2
Partisand <raydenn@wp.pl>
2019-10-21 20:20:11 +0200

    small steps forward

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
 	/**
 	* @Route("/")
@@ -22,10 +23,9 @@ class ArticleController
 
 	public function show($slug)
 	{
-		return new Response(sprintf(
-			'Future page to show the article: %s',
-			$slug
-		));
+		return $this->render('article/show.html.twig', [
+			'title' => ucwords(str_replace('-', ' ', $slug)),
+		]);
 	}	
 	
 }
